2895901a0ce9dd87e30cfffa7fb7a0b3624c8a57
Clement Stein <klemen31t@gmail.com>
2020-02-13 23:59:23 +0100

    gitignore + initial functionality

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,13 @@
+package com.example.demo.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Student {
+
+    private final Integer studentId;
+    private final String studentName;
+
+}
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+        new Student(1, "Anna"),
+        new Student(2, "Bob"),
+        new Student(3, "Charlie")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId){
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("User with id " + studentId + "does not exist."));
+    }
+}
