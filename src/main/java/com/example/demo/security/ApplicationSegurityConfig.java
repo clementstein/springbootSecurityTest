f3b9f519922b965df2f1f532b0f2bb7e886738f8
Clement Stein <klemen31t@gmail.com>
2020-02-16 20:56:29 +0100

    mvc config added

--- src/main/java/com/example/demo/security/ApplicationSegurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSegurityConfig.java
@@ -4,16 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 @Configuration
-@EnableWebSecurity
 public class ApplicationSegurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/", "index", "/static/**", "/css/**", "/js/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
